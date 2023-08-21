e1d26723de52ad184edf93be7bd65e8b9aa0eed3
Dasari-Rajesh <rdasari245@gmail.com>
2023-08-21 23:42:21 +0530

    Changes Forms

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -13,12 +13,23 @@ func routes(App *config.AppConfig) http.Handler {
 	// mux := pat.New()
 	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
 	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
-	mux := chi.NewRouter() 
+	mux := chi.NewRouter()
 	mux.Use(middleware.Recoverer)
 	mux.Use(NoSurf)
 	// mux.Use(SessionLoad)
 
 	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
 	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux.Get("/rooms/room1", http.HandlerFunc(handlers.Repo.Room1))
+
+	mux.Get("/rooms/room2", http.HandlerFunc(handlers.Repo.Room2))
+	mux.Get("/book", http.HandlerFunc(handlers.Repo.Book))
+	mux.Get("/contact", http.HandlerFunc(handlers.Repo.Contact))
+
+	mux.Get("/makeReserve", http.HandlerFunc(handlers.Repo.MakeReserve))
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
 	return mux
 }
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -10,6 +10,7 @@ import (
 
 var Repo *Repository
 var stringData = map[string]string{}
+
 type Repository struct {
 	App *config.AppConfig
 }
@@ -28,7 +29,7 @@ func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	remoteIP := r.RemoteAddr
 	// fmt.Println(remoteIP)
 	stringData["remote_ip"] = remoteIP
-	
+
 	// m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
 
 	// fmt.Println(remoteIP)
@@ -38,7 +39,6 @@ func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("About Running!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
-	
 	stringData["Test"] = "HELLO WOrLD"
 	// remoteIP := m.App.Session.GetString(r.Context(),"remote_ip")
 	// stringData["remote_ip"] = remoteIP
@@ -46,4 +46,51 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	renders.RenderTemplate(w, "about.page.html", &models.TemplateData{
 		StringData: stringData,
 	})
+}
+
+func (m *Repository) Room1(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Room 1 Running!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
+	// m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
+	// fmt.Println(remoteIP)
+
+	renders.RenderTemplate(w, "room1.page.html", &models.TemplateData{})
+}
+
+func (m *Repository) Room2(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Room 2 Running!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
+	// m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
+	// fmt.Println(remoteIP)
+
+	renders.RenderTemplate(w, "room2.page.html", &models.TemplateData{})
+}
+func (m *Repository) Book(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Book Running!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
+	// m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
+	// fmt.Println(remoteIP)
+
+	renders.RenderTemplate(w, "book.page.html", &models.TemplateData{})
+}
+func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Room 2 Running!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
+	// m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
+	// fmt.Println(remoteIP)
+
+	renders.RenderTemplate(w, "contact.page.html", &models.TemplateData{})
+}
+func (m *Repository) MakeReserve(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Reserve  Running!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
+	// m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
+	// fmt.Println(remoteIP)
+
+	renders.RenderTemplate(w, "makeReserve.page.html", &models.TemplateData{})
 }
